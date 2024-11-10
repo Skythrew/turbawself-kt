8f180c7fc72a83771900552b8d1801f7980a6fb2
Maël GUERIN <mael.guerin@murena.io>
2024-11-10 17:02:50 +0100

    feat(api): add `Balance` part

---
+++ library/src/commonMain/kotlin/ink/literate/turbawself/decoders/balance.kt
@@ -0,0 +1,18 @@
+package ink.literate.turbawself.decoders
+
+import ink.literate.turbawself.models.Balance
+import kotlinx.datetime.LocalDateTime
+import kotlinx.serialization.json.*
+
+fun decodeBalance (balance: JsonObject): Balance {
+    // Formatted as "Montant estimé au 12/08/2024"
+    val estimationDate = LocalDateTime.parse(balance["montantEstimeMsg"]!!.jsonPrimitive.content.slice(18..<balance["montantEstimeMsg"]!!.jsonPrimitive.content.length).split('/').reversed().joinToString("-") +  "T12:00:00")
+
+    return Balance(
+        id = balance["id"]!!.jsonPrimitive.content,
+        hostId = balance["hote"]!!.jsonObject["id"]!!.jsonPrimitive.long,
+        amount = balance["montant"]!!.jsonPrimitive.double,
+        estimationAfterBooks = balance["montantEstime"]!!.jsonPrimitive.double,
+        estimationDate
+    )
+}
