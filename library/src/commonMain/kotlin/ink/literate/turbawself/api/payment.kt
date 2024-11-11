fda52626dfad91655c21e083d1dfbb3d4eafd719
Maël GUERIN <mael.guerin@murena.io>
2024-11-11 17:58:53 +0100

    feat(api): add `payment`

---
+++ library/src/commonMain/kotlin/ink/literate/turbawself/api/payment.kt
@@ -0,0 +1,15 @@
+package ink.literate.turbawself.api
+
+import ink.literate.turbawself.core.Request
+import ink.literate.turbawself.decoders.decodePayment
+import ink.literate.turbawself.models.Authentication
+import ink.literate.turbawself.models.Payment
+import kotlinx.serialization.json.jsonObject
+
+suspend fun payment(auth: Authentication, token: String): Payment {
+    val request = Request(1, "paiements-payline/${token}")
+    request.useAuthentication(auth)
+    val content = request.send(auth.client)
+
+    return decodePayment(content.jsonObject)
+}
