f9f335b738375478a2a1af99e2db3d1520e313f6
Maël GUERIN <mael.guerin@murena.io>
2024-11-11 18:05:46 +0100

    feat(api): add `passwordReset`

---
+++ library/src/commonMain/kotlin/ink/literate/turbawself/api/password-reset.kt
@@ -0,0 +1,21 @@
+package ink.literate.turbawself.api
+
+import ink.literate.turbawself.core.Request
+import io.ktor.client.*
+import kotlinx.serialization.json.jsonArray
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
+
+suspend fun passwordReset(email: String, client: HttpClient = HttpClient()): Boolean {
+    val request = Request(1, "utilisateurs/password?email=$email")
+    val content = request.send(client)
+
+    if (content.jsonObject["rejected"]!!.jsonArray.size != 0)
+        throw Exception("Failed to send password reset email to ${
+            content.jsonObject["rejected"]!!.jsonArray.joinToString(
+                ","
+            ) { it.jsonPrimitive.content }
+        }")
+
+    return true
+}
