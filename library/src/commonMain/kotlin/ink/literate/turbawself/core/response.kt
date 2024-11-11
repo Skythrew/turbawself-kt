e7029ba06d316cb3b3749a477863c87e6d6ba0d4
Maël GUERIN <mael.guerin@murena.io>
2024-11-11 15:38:23 +0100

    fix(response): add empty response handling

--- library/src/commonMain/kotlin/ink/literate/turbawself/core/response.kt
+++ library/src/commonMain/kotlin/ink/literate/turbawself/core/response.kt
@@ -4,7 +4,12 @@ import io.ktor.client.statement.*
 import kotlinx.serialization.json.*
 
 suspend fun handleResponse(response: HttpResponse): JsonElement {
-    val content = Json.parseToJsonElement(response.bodyAsText())
+    val responseBody = response.bodyAsText()
+
+    if (responseBody == "")
+        return Json.parseToJsonElement("null")
+
+    val content = Json.parseToJsonElement(responseBody)
 
     if (response.status.value < 200 || response.status.value > 299) {
         throw Exception("${content.jsonObject["message"]!!.jsonPrimitive.content} (${response.status.value})")
