eb8d5759e2805049bff6360caeba4293afce3e34
Maël GUERIN <mael.guerin@murena.io>
2024-11-11 14:46:33 +0100

    fix(balance): return list of balances

--- library/src/commonMain/kotlin/ink/literate/turbawself/api/balances.kt
+++ library/src/commonMain/kotlin/ink/literate/turbawself/api/balances.kt
@@ -8,12 +8,12 @@ import ink.literate.turbawself.models.Session
 import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonObject
 
-suspend fun balance (auth: Authentication, session: Session): Balance {
+suspend fun balances (auth: Authentication, session: Session): List<Balance> {
     // NOTE: Not sure why the `1` is appended to the
     // URL path since it also returns the same output with /0, /2, ...
     val request = Request(1, "comptes/hotes/${session.hostID}/1")
     request.useAuthentication(auth)
     val content = request.send(auth.client)
 
-    return decodeBalance(content.jsonArray[0].jsonObject)
+    return content.jsonArray.map { decodeBalance(it.jsonObject) }
 }
