18bf71bb11fb43da395b3a675e87013aba77e303
Maël GUERIN <mael.guerin@murena.io>
2024-11-11 16:45:10 +0100

    fix(BookingDay): add nullability for `id` member property

--- library/src/commonMain/kotlin/ink/literate/turbawself/models/BookingDay.kt
+++ library/src/commonMain/kotlin/ink/literate/turbawself/models/BookingDay.kt
@@ -3,7 +3,7 @@ package ink.literate.turbawself.models
 import kotlinx.datetime.LocalDateTime
 
 data class BookingDay(
-    val id: String,
+    val id: String? = null,
     val booked: Boolean,
     val canBook: Boolean,
     val dayNumber: Short,
