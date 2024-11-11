f7287f3d1049b21fd611edd9bf58e7219c19eb43
Maël GUERIN <mael.guerin@murena.io>
2024-11-11 17:40:15 +0100

    feat(api): add `history`

---
+++ library/src/commonMain/kotlin/ink/literate/turbawself/api/history.kt
@@ -0,0 +1,17 @@
+package ink.literate.turbawself.api
+
+import ink.literate.turbawself.core.Request
+import ink.literate.turbawself.decoders.decodeHistoryPayment
+import ink.literate.turbawself.models.Authentication
+import ink.literate.turbawself.models.HistoryPayment
+import ink.literate.turbawself.models.Session
+import kotlinx.serialization.json.jsonArray
+import kotlinx.serialization.json.jsonObject
+
+suspend fun history (auth: Authentication, session: Session): List<HistoryPayment> {
+    val request = Request(1, "historiques/hotes/${session.hostID}")
+    request.useAuthentication(auth)
+    val content = request.send(auth.client)
+
+    return content.jsonArray.map { decodeHistoryPayment(it.jsonObject) }
+}
